35485fbc1aadb497dce4cf6e00c7a9110117a0c8
Mark Creasy <mcreasy50@gmail.com>
2022-05-25 11:45:06 -0400

    setup db connection

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
